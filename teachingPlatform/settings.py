0a067cf3366f386757c35f3c9ef9135cd11af16a
Gabriel Palma <gabrielrpalma2@gmail.com>
2021-08-08 13:17:45 -0300

    version 6

--- teachingPlatform/settings.py
+++ teachingPlatform/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMeddeware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'teachingPlatform.urls'
